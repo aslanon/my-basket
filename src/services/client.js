10f5b66a62bad6d0c3351ad1c044f0cb09e56f46
aslanon <aslanon.tr@gmail.com>
2020-08-12 20:40:13 +0300

    Add client interceptors.

--- src/services/client.js
+++ src/services/client.js
@@ -11,6 +11,19 @@ const clientInstance = baseURL =>
 
 const client = clientInstance(process.env.VUE_APP_API_BASE_URL)
 
+client.interceptors.response.use(
+  response => response,
+  error => {
+    if (error && error.response) {
+      const {
+        response: { status }
+      } = error
+
+      console.log(error, status)
+    }
+  }
+)
+
 export default {
   install() {
     Vue.prototype.$client = client
